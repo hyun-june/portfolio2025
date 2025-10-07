4c9e0182d5048ff2889ce12d4e6741ac720ec2e4
Hyun-june <leehyunjune11@naver.com>
2025-10-07 15:37:00 +0900

    style

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,11 @@
+import "./Profile.css";
+
+const Profile = () => {
+  return (
+    <div className="profile_card">
+      <div className="profile_img">이미지</div>
+    </div>
+  );
+};
+
+export default Profile;
